fab458eead724c752d58bb9b42558e0e2245d8ee
Daniel McNaughton <danieljmcnaughton@gmail.com>
2019-09-08 22:56:32 -0400

    Initial commit.

---
+++ System.Linq.HasDuplicates/System.Linq.HasDuplicates.Benchmark/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace System.Linq.HasDuplicates.Benchmark
+{
+    [InProcessAttribute]
+    public class DuplicatesBenchmark
+    {
+        private List<string> dataWithoutDuplicates;
+
+        private List<string> dataWithDuplicates;
+
+        [Params(100,  100000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var fullWordList = File.ReadLines("words.txt").ToArray();
+            dataWithoutDuplicates = new Span<string>(fullWordList, 0, N).ToArray().ToList();
+
+            dataWithDuplicates = new Span<string>(fullWordList, 0, N/2).ToArray().ToList();
+            dataWithDuplicates.AddRange(new Span<string>(fullWordList, 0, N / 2).ToArray().ToList());
+
+        }
+
+        [Benchmark]
+        public bool LinqExtensionHasDuplicatesWithDuplicates() => dataWithDuplicates.HasDuplicates();
+
+        [Benchmark]
+        public bool LinqGroupByWithDuplicates() => dataWithDuplicates.GroupBy(x => x).Any(x => x.Count() > 1);
+
+        [Benchmark]
+        public bool LinqCountDistinctWithDuplicates() => dataWithDuplicates.Count == dataWithDuplicates.Distinct().Count();
+
+        [Benchmark]
+        public bool LinqExtensionHasDuplicatesWithoutDuplicates() => dataWithoutDuplicates.HasDuplicates();
+
+        [Benchmark]
+        public bool LinqGroupByWithoutDuplicates() => dataWithoutDuplicates.GroupBy(x => x).Any(x => x.Count() > 1);
+
+        [Benchmark]
+        public bool LinqCountDistinctWithoutDuplicates() => dataWithoutDuplicates.Count == dataWithDuplicates.Distinct().Count();
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<DuplicatesBenchmark>();
+        }
+    }
+}
---
+++ System.Linq.HasDuplicates/System.Linq.HasDuplicates/Duplicates.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Linq.HasDuplicates
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Returns true if two or more elements within the <paramref name="source"/> are the same.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool HasDuplicates<TSource>(this IEnumerable<TSource> source) => HasDuplicates(source, null);
+
+        /// <summary>
+        /// Returns true if two or more elements within the <paramref name="source"/> are the same.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static bool HasDuplicates<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
+        {
+            // Null objects don't have duplicates.
+            if (source == null)
+            {
+                return false;
+            }
+
+            var set = new Set<TSource>(comparer);
+
+            foreach (var element in source)
+            {
+                if (!set.Add(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns back an enumerable list of elements that were duplicates, as well as the count for each element instance in the source.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<TSource, int>> Duplicates<TSource>(this IEnumerable<TSource> source) => Duplicates(source, null);
+
+        /// <summary>
+        /// Returns back an enumerable list of elements that were duplicates, as well as the count for each element instance in the source.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<TSource, int>> Duplicates<TSource>(this IEnumerable<TSource> source,
+            IEqualityComparer<TSource> comparer)
+        {
+            // Null objects don't have duplicates, but we don't like throwing null value exceptions. Maybe if we null check this method it won't throw an exception.
+            // Narrator(Morgan Freeman): It does throw an exception.
+            if (source == null)
+            {
+                return Enumerable.Empty<KeyValuePair<TSource, int>>();
+            }
+
+            var set = new Set<TSource>(comparer);
+
+            foreach (var element in source)
+            {
+                set.Add(element);
+            }
+
+            return set.ToEnumerableWithCount();
+        }
+    }
+}
